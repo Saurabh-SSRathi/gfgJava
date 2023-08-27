70bf8660f057e3cedad2ce3e255a4da7ad556207
Saurabh-SSRathi <saurabhss.rathi@gmail.com>
2023-08-27 13:46:43 +0530

    Que1M4LargestElement

--- src/array/Que1M4LargestElement.java
+++ src/array/Que1M4LargestElement.java
@@ -8,8 +8,8 @@ public class Que1M4LargestElement {
     public static void main(String[] args) {
         int[] arr = {12 ,2, 54, 12, 67};
         List<Integer> list = new ArrayList<>();
-        for (int i=0; i<arr.length; i++) {
-            list.add(arr[i]);
+        for (int j : arr) {
+            list.add(j);
         }
         System.out.println("Largest in given array is " + Collections.max(list));
     }
