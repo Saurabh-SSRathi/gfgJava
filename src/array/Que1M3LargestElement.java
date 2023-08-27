6b0ee1225f17fb8a5cde01944dcefe0da6913e73
Saurabh-SSRathi <saurabhss.rathi@gmail.com>
2023-08-27 13:36:09 +0530

    Que1M3LargestElement

---
+++ src/array/Que1M3LargestElement.java
@@ -0,0 +1,21 @@
+package array;
+
+import java.util.Arrays;
+
+public class Que1M3LargestElement {
+    static int largest(int[] arr, int n) {
+        Arrays.sort(arr);
+        return arr[n-1];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12 ,2, 54, 12, 67};
+        int n = arr.length;
+        System.out.println("Largest in given array is " + largest(arr,n));
+    }
+
+    /*Note:
+      Time Complexity: O(n), where n represents the size of the given array.
+      Auxiliary Space: O(1), no extra space is required, so it is a constant.
+     */
+}
