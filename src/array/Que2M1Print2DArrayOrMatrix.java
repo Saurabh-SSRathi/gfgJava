a3929b71ea2bd5e39e73b67bcb821045a200c3ee
Saurabh-SSRathi <saurabhss.rathi@gmail.com>
2023-08-29 23:56:38 +0530

    Que2M1Print2DArrayOrMatrix

--- src/array/Que2M1Print2DArrayOrMatrix.java
+++ src/array/Que2M1Print2DArrayOrMatrix.java
@@ -1,7 +1,7 @@
 package array;
 
 public class Que2M1Print2DArrayOrMatrix {
-    public static void print2DMatrix(int mat[][]) {
+    public static void print2DMatrix(int[][] mat) {
 
         // Loop through all rows
         for (int i=0; i<mat.length; i++) {
@@ -13,7 +13,7 @@ public class Que2M1Print2DArrayOrMatrix {
     }
 
     public static void main(String[] args) {
-        int mat[][] = { {1, 2, 3, 4},
+        int[][] mat = { {1, 2, 3, 4},
                 {5, 6, 7, 8},
                 {8, 9, 10, 11}};
         print2DMatrix(mat);
