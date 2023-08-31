36d3d191650013609e5677cd90e86a22438c3ff9
Saurabh-SSRathi <saurabhss.rathi@gmail.com>
2023-08-31 23:58:57 +0530

    31-08-2023

--- src/array/Que2M3Print2DArrayOrMatrix.java
+++ src/array/Que2M3Print2DArrayOrMatrix.java
@@ -1,26 +1,32 @@
+// Java program to print the elements of
+// a 2 D array or matrix using for-each
 package array;
 
-public class Que2M1Print2DArrayOrMatrix {
+import java.util.Arrays;
+
+public class Que2M3Print2DArrayOrMatrix {
     public static void print2DMatrix(int[][] mat) {
 
         // Loop through all rows
-        for (int i=0; i<mat.length; i++) {
+        for (int[] row : mat) {
             // Loop through all elements of current row
-            for (int j=0; j<mat[i].length; j++) {
-                System.out.print(mat[i][j] + " ");
-            }
+
+            // converting each row as string and then printing in a separate line
+            System.out.println(Arrays.toString(row));
         }
     }
 
     public static void main(String[] args) {
         int[][] mat = { {1, 2, 3, 4},
                 {5, 6, 7, 8},
-                {8, 9, 10, 11}};
+                {9, 10, 11, 12}};
         print2DMatrix(mat);
     }
 }
 
-/* Output : 1 2 3 4 5 6 7 8 8 9 10 11 */
+/* Output : [1, 2, 3, 4]
+            [5, 6, 7, 8]
+            [9, 10, 11, 12] */
 
 /*
 Time Complexity: O(N*M) where N is the number of rows in the matrix and M is the number of columns in the matrix.
