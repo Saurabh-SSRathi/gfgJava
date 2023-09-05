440d1aab1a2bd152f6e1417c13029c6372a71469
Saurabh-SSRathi <saurabhss.rathi@gmail.com>
2023-09-05 23:51:28 +0530

    05-09-2023

--- src/array/Que3M1ArrayCopy.java
+++ src/array/Que3M1ArrayCopy.java
@@ -16,14 +16,14 @@ public class Que3M1ArrayCopy {
 
         // Changing b[] to verify that
         // b[] is different from a[]
-            b[0]++;
+        b[0]++;
 
-            System.out.println("Contents of a[] ");
-            for ( int i = 0; i< a.length; i++)
-                System.out.print(a[i] + " ");
+        System.out.println("Contents of a[] ");
+        for ( int i = 0; i< a.length; i++)
+            System.out.print(a[i] + " ");
 
-            System.out.println("\n\nContents of b[] ");
-            for ( int i = 0; i < b.length; i++)
-                System.out.print(b[i] + " ");
+        System.out.println("\n\nContents of b[] ");
+        for ( int i = 0; i < b.length; i++)
+            System.out.print(b[i] + " ");
     }
 }
